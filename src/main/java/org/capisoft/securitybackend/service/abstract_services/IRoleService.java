5708955c9431b248fff3e608ea460f6d8a189d43
suarezKevin <suarezk453@gmail.com>
2024-01-13 09:47:08 -0500

    Get roles service

--- src/main/java/org/capisoft/securitybackend/service/abstract_services/IRoleService.java
+++ src/main/java/org/capisoft/securitybackend/service/abstract_services/IRoleService.java
@@ -1,2 +1,10 @@
-package org.capisoft.securitybackend.service.abstract_services;public interface IRoleService {
+package org.capisoft.securitybackend.service.abstract_services;
+
+import org.capisoft.securitybackend.common.CustomAPIResponse;
+import org.springframework.http.ResponseEntity;
+
+public interface IRoleService {
+
+    ResponseEntity<CustomAPIResponse<?>> getRoles();
+
 }
