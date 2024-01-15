a3bae17593c8f34bbb100899b73fd356155cd965
Alex Tigselema <kaiser.alex1994@gmail.com>
2024-01-15 10:43:32 -0500

    Template services with folders and student filters

--- src/main/java/org/capisoft/securitybackend/service/services/TemplateService.java
+++ src/main/java/org/capisoft/securitybackend/service/services/TemplateService.java
@@ -6,15 +6,9 @@ import org.capisoft.securitybackend.api.models.requests.TemplateRequest;
 import org.capisoft.securitybackend.api.models.responses.TemplateResponse;
 import org.capisoft.securitybackend.common.CustomAPIResponse;
 import org.capisoft.securitybackend.common.CustomResponseBuilder;
-import org.capisoft.securitybackend.entities.Career;
-import org.capisoft.securitybackend.entities.CareerAcademicPeriod;
-import org.capisoft.securitybackend.entities.Folder;
-import org.capisoft.securitybackend.entities.Template;
+import org.capisoft.securitybackend.entities.*;
 import org.capisoft.securitybackend.mappers.TemplateMapper;
-import org.capisoft.securitybackend.repositories.CareerAcademicPeriodRepository;
-import org.capisoft.securitybackend.repositories.CareerRepository;
-import org.capisoft.securitybackend.repositories.FolderRepository;
-import org.capisoft.securitybackend.repositories.TemplateRepository;
+import org.capisoft.securitybackend.repositories.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -32,12 +26,14 @@ public class TemplateService {
     private final CareerRepository careerRepository;
     private final FolderRepository folderRepository;
 
+    private final StudentRepository studentRepository;
     private final CustomResponseBuilder responseBuilder;
-    public TemplateService(TemplateRepository templateRepository, CareerAcademicPeriodRepository careerAcademicPeriodRepository, CareerRepository careerRepository, FolderRepository folderRepository, CustomResponseBuilder responseBuilder) {
+    public TemplateService(TemplateRepository templateRepository, CareerAcademicPeriodRepository careerAcademicPeriodRepository, CareerRepository careerRepository, FolderRepository folderRepository, StudentRepository studentRepository, CustomResponseBuilder responseBuilder) {
         this.templateRepository = templateRepository;
         this.careerAcademicPeriodRepository = careerAcademicPeriodRepository;
         this.careerRepository = careerRepository;
         this.folderRepository = folderRepository;
+        this.studentRepository = studentRepository;
         this.responseBuilder = responseBuilder;
     }
 
@@ -68,4 +64,12 @@ public class TemplateService {
         List<TemplateResponse> responses = templates.stream().map(TemplateMapper::templateResponseFromTemplate).toList();
         return responseBuilder.buildResponse(HttpStatus.OK, "Templates encontrados exitosamente!", responses);
     }
+
+    public ResponseEntity<CustomAPIResponse<?>> findByStudentId(Long id){
+        Student student = studentRepository.findById(id).orElseThrow(() -> new RuntimeException("Estudiante no encontrado."));
+        List<Template> templates = templateRepository.findAllByCareerAcademicPeriodsIn(student.getCareerAcademicPeriods());
+        List<TemplateResponse> responses = templates.stream().map(TemplateMapper::templateResponseFromTemplate).toList();
+        return responseBuilder.buildResponse(HttpStatus.OK, "Templates encontrados exitosamente!", responses);
+    }
+
 }
