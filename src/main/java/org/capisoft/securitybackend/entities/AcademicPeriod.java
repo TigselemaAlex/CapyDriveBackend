356680008a35b4ce85269da15535afcea420f28b
suarezKevin <suarezk453@gmail.com>
2024-01-15 14:53:21 -0500

    Adjustment in AcademicPeriod entity

--- src/main/java/org/capisoft/securitybackend/entities/AcademicPeriod.java
+++ src/main/java/org/capisoft/securitybackend/entities/AcademicPeriod.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Builder
 public class AcademicPeriod {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private LocalDate startDate;
