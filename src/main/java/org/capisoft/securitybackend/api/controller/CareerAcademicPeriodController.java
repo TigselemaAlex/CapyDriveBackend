de786eff7f129216e6e337bde85e2d19cfd63d40
suarezKevin <suarezk453@gmail.com>
2024-01-14 20:39:15 -0500

    Adjustment

--- src/main/java/org/capisoft/securitybackend/api/controller/CareerAcademicPeriodController.java
+++ src/main/java/org/capisoft/securitybackend/api/controller/CareerAcademicPeriodController.java
@@ -2,7 +2,6 @@ package org.capisoft.securitybackend.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import org.capisoft.securitybackend.api.models.requests.AcademicPeriodRequest;
 import org.capisoft.securitybackend.api.models.requests.CareerAcademicPeriodRequest;
 import org.capisoft.securitybackend.common.CustomAPIResponse;
 import org.capisoft.securitybackend.service.abstract_services.ICareerAcademicPeriodService;
